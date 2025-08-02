bf18907c3d65ed360831dbaa497ead3c3bfa828e
Jacky <zhongzhenyu190@gmail.com>
2025-08-02 01:15:28 -0400

    refactor: refacotr backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import Navigation from "@/components/sections/navigation";
 import Hero from "@/components/sections/hero";
+
 export default function Home() {
   return (
     <div className="bg-primary-background overflow-x-hidden">
